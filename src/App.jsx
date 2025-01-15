7ae50c940bca29d0b229aecf9215ae51db1dc64b
Abhinav saini <abhinavsaini622@gmail.com>
2025-01-15 21:20:58 +0530

    new update again

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,11 @@ import './App.css'
 import Navbar from './component/Navbar'
 import Header from './component/Header'
 import About from './component/About'
-import Test from './component/Test'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import Projects from './component/Projects'
 import Footer from './component/Footer'
 import Skill from './component/Skill'
 
 function App() {
-
   return (
     <div>
       <Navbar/>
